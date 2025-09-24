8fbac39f38a4a85a82cc62dfe2e7ae1e36a31831
Anikavuk <usla_2018@mail.ru>
2025-09-24 12:42:16 +0500

    fix: fixed page loading paths

--- app/api/endpoints/pages.py
+++ app/api/endpoints/pages.py
@@ -1,27 +1,28 @@
 
 from fastapi import APIRouter, Request
-from fastapi.responses import FileResponse, PlainTextResponse
-
+from fastapi.templating import Jinja2Templates
 from app.services.motivation_ai import HuggingFacePredictor
+from fastapi.responses import HTMLResponse
 
 router = APIRouter()
+templates = Jinja2Templates(directory="app/templates")
 
-
-@router.get("/")
-def index():
+@router.get("/", response_class=HTMLResponse)
+def index(request: Request):
     """Метод загрузки стартовой страницы"""
-    return FileResponse("app/templates/index.html")
+    return templates.TemplateResponse("index.html", {"request": request})
 
-
-@router.post("/get_prediction")
-def get_prediction(request: Request):
+@router.post("/get_prediction", response_class=HTMLResponse)
+async def get_prediction(request: Request):
     """Метод загрузки страницы с предсказанием"""
     predictor = HuggingFacePredictor()
-    response = predictor.get_prediction()
-    return PlainTextResponse(content=response)
+    response_text = predictor.get_prediction()
+    return templates.TemplateResponse(
+        "get_prediction.html",
+        {"request": request, "prediction": response_text}
+    )
 
-
-@router.post("/questions")
-def questions():
+@router.post("/questions", response_class=HTMLResponse)
+def questions(request: Request):
     """Метод загрузки анкеты пользователя"""
-    return FileResponse("app/templates/questions.html")
+    return templates.TemplateResponse("questions.html", {"request": request})
