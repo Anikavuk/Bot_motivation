c27c4287daaefdcb3a80632ba546c208da54d3b7
prodream <sushkoos@gmail.com>
2025-11-29 09:25:23 +0400

    small fixes

--- tests/test_get_prediction.py
+++ tests/test_get_prediction.py
@@ -21,9 +21,7 @@ async def test_get_prediction_creates_user_and_new_prediction(
 
     # Настройка моков
     mock_user_service.get_user_by_uuid.return_value = None
-    mock_user_service.get_date_prediction.return_value = datetime(
-        2025, 10, 20, tzinfo=timezone.utc
-    )
+    mock_user_service.get_date_prediction.return_value = datetime(2025, 10, 20, tzinfo=timezone.utc)
     mock_prediction_service.get_prediction_for_user.return_value = None
 
     fake_user = AsyncMock()
@@ -32,13 +30,9 @@ async def test_get_prediction_creates_user_and_new_prediction(
     mock_user_service.create_user.return_value = fake_user
 
     # мокаем HuggingFacePredictor
-    with patch.object(
-        HuggingFacePredictor, "get_prediction", return_value=fake_prediction_text
-    ):
+    with patch.object(HuggingFacePredictor, "get_prediction", return_value=fake_prediction_text):
         transport = ASGITransport(app=web_app)
-        async with AsyncClient(
-            transport=transport, base_url="http://test"
-        ) as async_client:
+        async with AsyncClient(transport=transport, base_url="http://test") as async_client:
             await async_client.get("/")
             response = await async_client.post("/get_prediction", data={"name": name})
 
